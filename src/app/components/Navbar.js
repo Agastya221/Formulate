71f78e9a9b690318a634d911ac0cba45a6d998cc
agastya221 <agastyash213@gmail.com>
2024-04-13 12:24:33 +0530

    Changes the kinks

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -15,14 +15,14 @@ export default function Navbar() {
           priority
         />
       <li className='space-x-6 list-none text-purple-400 cursor-pointer font-medium'>
-        <Link href='#'>Home</Link>
-        <Link href='#'>Profile</Link>
-        <Link href='#'>Pricing</Link>
-        <Link href='#'>Careers</Link>
+        <Link href='/'>Home</Link>
+        <Link href='/'>Profile</Link>
+        <Link href='/'>Pricing</Link>
+        <Link href='/'>Careers</Link>
       </li>
       <li className='space-x-6  list-none cursor-pointer font-medium'>
-        <Link className='text-purple-400 hover:text-purple-200'  href='/login'>Log in</Link>
-        <Link className='rounded bg-purple-600 py-2 px-4 hover:bg-purple-700 transition duration-200' href='/signup'>Sign up</Link>
+        <Link className='text-purple-400 hover:text-purple-200'  href='/'>Log in</Link>
+        <Link className='rounded bg-purple-600 py-2 px-4 hover:bg-purple-700 transition duration-200' href='/'>Sign up</Link>
         
       </li>
     </div>
